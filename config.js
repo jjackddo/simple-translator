7dd111b5267c52caffd20f04e823a06009bc5a4b
jjackddo <kevinpark83@gmail.com>
2026-04-24 17:21:59 +0900

    config.js: 브라우저용 Translation API 키 추가 (referrer 제한됨)

--- config.js
+++ config.js
@@ -11,5 +11,5 @@
 // 제한 덕분에 키가 유출돼도 허용된 도메인 외에서는 사용 불가능합니다.
 
 window.APP_CONFIG = {
-  GOOGLE_API_KEY: ""   // ← 여기에 브라우저용 API 키를 붙여넣으세요 (AIza...)
+  GOOGLE_API_KEY: "AIzaSyAtL8aL6u8UJ_QMUC3UnV99fW6PkBpELro"   // ← 여기에 브라우저용 API 키를 붙여넣으세요 (AIza...)
 };
